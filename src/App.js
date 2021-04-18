40f14d04cc97ec18f71ec4ec27529ddca7fe01f7
sadatakhtar <sadat0akhtar@gmail.com>
2021-04-18 16:04:29 +0100

    fixed API issue

--- src/App.js
+++ src/App.js
@@ -5,30 +5,13 @@ import Header from './components/headerComp/Header'
 import MainPage from './pages/MainPage';
 
 function App() {
+
   const [searcher, setSearcher] = useState(false);
-  const [headLine, setHeadLine] = useState('');
-    
-    useEffect(() => {
-        fetch('http://127.0.0.7:3008/news')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            //console.log(data[0].title);
-            setHeadLine(data);
-            console.log(headLine);
-        })
-        .catch(err => {
-            console.error(err);
-        })
-
-    }, [headLine])
-
   
   return (
     <div className="App">
       <Header searcher={searcher} setSearcher={setSearcher}/>
-      <MainPage searcher={searcher} setSearcher={setSearcher} headLine={headLine}/>
+      <MainPage searcher={searcher} setSearcher={setSearcher}  />
       <Footer />
     </div>
   );
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './MainPage.css'
 import border from '../assets/Line.png'
 import image1 from '../assets/tree.jpeg'
 import image2 from '../assets/scene.jpg'
 import image3 from '../assets/ltcode.jpg'
 
+const axios = require('axios');
 
-function MainPage({searcher, setSearcher, headLine}) {
+function MainPage({searcher, setSearcher}) {
+
+    const [headLine, setHeadLine] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('http://127.0.0.1:3007/news');
+      console.log(headLine);
+      setHeadLine(res.data);
+      setIsLoading(true);
+      console.log(headLine);
+    };
     
-    return (
+    fetchData();
+   
+}, []);
+  
+    
+   
+    return !isLoading ? (<p>Loading....</p>) : (
         <div className="mainpage_container">
          
             <div className="mainpage_heading">
@@ -21,13 +41,13 @@ function MainPage({searcher, setSearcher, headLine}) {
             <div className="mainpage_wrapper">
                 <div className="mainpage_left">
                     <div className="mainpage_left_box1">
-                        <h3>{headLine[0].source.name}</h3>
+                        <h3>{headLine[7].source.name}</h3>
                         <div className="box1_inner">
-                            <h2>{headLine[0].title}</h2>
-                            <img src={headLine[0].urlToImage} alt="pic" width="350"/>
+                             <h2>{headLine[7].title}</h2>
+                             <img src={headLine[7].urlToImage} alt="pic" width="350"/>
                         </div>
-                        <p>{headLine[0].description}</p>
-                        <h4>{headLine[0].publishedAt}</h4>
+                         <p>{headLine[7].description}</p>
+                         <h4>{headLine[7].publishedAt}</h4>
 
                     </div>
                   
@@ -42,14 +62,14 @@ function MainPage({searcher, setSearcher, headLine}) {
                     </div>
                     <div className="mainpage_left_sub_wrapper">
                         <div className="bottom_box1">
-                            <h3 className="bottom_box_small_title">Brexit</h3>
-                            <h3>EU and UK head towards a trade deal for Northern Ireland</h3>
-                            <p>Just some random text here which will get replaced once the API is attached</p>
+                            <h3 className="bottom_box_small_title">{headLine[6].source.name}</h3>
+                            <h3>{headLine[6].title}</h3>
+                            <p>{headLine[6].description}</p>
                         </div>
                         <div className="bottom_box2">
-                        <h3 className="bottom_box_small_title">Brexit</h3>
-                            <h3>EU and UK head towards a trade deal for Northern Ireland</h3>
-                            <p>Just some random text here which will get replaced once the API is attached</p>
+                        <h3 className="bottom_box_small_title">{headLine[5].source.name}</h3>
+                        <h3>{headLine[5].title}</h3>
+                            <p>{headLine[5].description}</p>
                         </div>
                     </div>
                    
@@ -62,21 +82,21 @@ function MainPage({searcher, setSearcher, headLine}) {
                    
                     <h3>Recommended</h3>
                     <div className="sideboxes">
-                        <img src={image1} alt="tree " width="300px"/>
-                        <h3>title</h3>
-                        <p>Just some wording here describing the image</p>   
+                        <h3>{headLine[4].source.name}</h3>
+                        <img src={headLine[4].urlToImage} alt="tree " width="300px"/>
+                        <p>{headLine[4].title}</p>   
                     </div>
 
                     <div className="sideboxes">
-                        <img src={image2} alt="scenic" width="300px"/>
-                        <h3>title</h3>
-                        <p>Just some wording here describing the image</p>   
+                    <h3>{headLine[2].source.name}</h3>
+                        <img src={headLine[2].urlToImage} alt="scenic" width="300px"/>
+                        <p>{headLine[2].title}</p>   
                     </div>
 
                     <div className="sideboxes">
-                        <img src={image3} alt="" width="300px"/>
-                        <h3>title</h3>
-                        <p>Just some wording here describing the image</p>   
+                        <h3>{headLine[1].source.name}</h3>
+                        <img src={headLine[1].urlToImage} alt="" width="300px"/>
+                        <p>{headLine[1].title}</p>   
                     </div>
                     <div className="borders">
                         <img src={border} alt="line" />
