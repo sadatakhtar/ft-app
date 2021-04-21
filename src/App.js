fdda2f918c7d5f0093829f312ebc102ebda981cc
sadatakhtar <sadat0akhtar@gmail.com>
2021-04-21 12:46:26 +0100

    finished search input functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Footer from './components/footerComp/Footer';
 import Header from './components/headerComp/Header'
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './MainPage.css'
 import border from '../assets/Line.png'
-import image1 from '../assets/tree.jpeg'
-import image2 from '../assets/scene.jpg'
-import image3 from '../assets/ltcode.jpg'
+
 
 const axios = require('axios');
 
@@ -11,22 +9,43 @@ function MainPage({searcher, setSearcher}) {
 
     const [headLine, setHeadLine] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+    const [newSearchValue, setNewSearchValue] = useState('');
 
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get('http://127.0.0.1:3007/news');
-      console.log(headLine);
       setHeadLine(res.data);
       setIsLoading(true);
       console.log(headLine);
+     
     };
     
     fetchData();
    
 }, []);
-  
-    
+
+ console.log(searchValue);
+ 
+    //FUNCTION TO HANDLE THE SEARCH VALUE
+    const handleSearch = (e) => {
+        setSearchValue(e.target.value);
+        
+
+        setNewSearchValue(headLine.filter(({title, description, content, source}) => {
+            // return (title.toLowerCase().includes(searchValue) || 
+            // description.toLowerCase().includes(searchValue)) ||
+            // content.toLowerCase().includes(searchValue) ||
+          return   source.name.toLowerCase().includes(searchValue) ||
+            title.toLowerCase().includes(searchValue);
+
+        }))
+      
+    }
+
+    // console.log(headLine);
+    // console.log(newSearchValue);
    
     return !isLoading ? (<p>Loading....</p>) : (
         <div className="mainpage_container">
@@ -35,19 +54,19 @@ function MainPage({searcher, setSearcher}) {
                 <h2>UK</h2>   
             </div>
             <div className={searcher ? "searchbox": "toggle"}>
-                 <input type="text" placeholder="Search..." />
-                 <button type="submit">Search</button>
+                 <input type="text" placeholder="Search..." onChange={handleSearch}/>
+                 {/* <button type="submit" onClick={handleSearchBtn}>Search</button> */}
             </div>
             <div className="mainpage_wrapper">
                 <div className="mainpage_left">
                     <div className="mainpage_left_box1">
-                        <h3>{headLine[7].source.name}</h3>
+                        <h3>{newSearchValue.length > 1 ? (newSearchValue[0].source.name) : (headLine[7].source.name)}</h3>
                         <div className="box1_inner">
-                             <h2>{headLine[7].title}</h2>
-                             <img src={headLine[7].urlToImage} alt="pic" width="350"/>
+                             <h2>{newSearchValue.length > 1 ? (newSearchValue[0].title) : (headLine[7].title)}</h2>
+                             <img src={newSearchValue.length > 1 ? (newSearchValue[0].urlToImage) : (headLine[7].urlToImage)} alt="pic" width="350"/>
                         </div>
-                         <p>{headLine[7].description}</p>
-                         <h4>{headLine[7].publishedAt}</h4>
+                         <p>{newSearchValue.length > 1 ? (newSearchValue[0].description) : (headLine[7].description)}</p>
+                         <h4>{newSearchValue.length > 1 ? (newSearchValue[0].publishedAt) : (headLine[7].publishedAt)}</h4>
 
                     </div>
                   
@@ -62,9 +81,9 @@ function MainPage({searcher, setSearcher}) {
                     </div>
                     <div className="mainpage_left_sub_wrapper">
                         <div className="bottom_box1">
-                            <h3 className="bottom_box_small_title">{headLine[6].source.name}</h3>
-                            <h3>{headLine[6].title}</h3>
-                            <p>{headLine[6].description}</p>
+                            <h3 className="bottom_box_small_title">{newSearchValue.length > 1 ? (newSearchValue[1].source.name) : (headLine[6].source.name)}</h3>
+                            <h3>{newSearchValue.length > 1 ? (newSearchValue[1].title) : (headLine[6].title)}</h3>
+                            <p>{newSearchValue.length > 1 ? (newSearchValue[1].description) : (headLine[6].description)}</p>
                         </div>
                         <div className="bottom_box2">
                         <h3 className="bottom_box_small_title">{headLine[5].source.name}</h3>
